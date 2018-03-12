3628ba970b8c0e43557836cc1a7a71540421e3ea
clarissenshuti <clarisse.nshuti@student.pxl.be>
2018-03-12 17:02:21 +0100

    Add files via upload
    database name test
    table name persons

---
+++ test.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 12, 2018 at 03:58 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.25-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 
+Table structure for table `persons`
+--
+
+
+CREATE TABLE `persons` (
+ 
+ `PersonID` int(4) NOT NULL,
+ 
+ `FirstName` varchar(30) NOT NULL,
+ 
+ `LastName` varchar(30) NOT NULL,
+ 
+ `Email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
+--
+ Dumping data for table `persons`
+--
+
+
+INSERT INTO `persons` (`PersonID`, `FirstName`, `LastName`, `Email`) 
+VALUES
+(1, 'JaneB', 'Doe', 'jane.doe@example.com'),
+
+(2, 'John', 'Doe', 'john.doe@pxl.be'),
+
+(3, 'Het', 'Regent', 'hetRegent@buiten.be');
+
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
